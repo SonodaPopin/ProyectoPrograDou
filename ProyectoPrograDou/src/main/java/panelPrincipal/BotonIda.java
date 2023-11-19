9523f0890acbc2c31016cd6c64d714792a09dcb5
SonodaPopin <joahernandez2022@udec.cl>
2023-11-18 22:32:09 -0300

    Adicion de algunas texturas en Boton Ida y Boton Vuelta

--- ProyectoPrograDou/src/main/java/panelPrincipal/BotonIda.java
+++ ProyectoPrograDou/src/main/java/panelPrincipal/BotonIda.java
@@ -7,6 +7,18 @@ import java.awt.event.*;
 public class BotonIda extends JPanel {
     BotonIda(){
         this.setBounds(50,400,300,100);
-        this.setBackground(Color.GREEN);
+        this.setBackground(Color.LIGHT_GRAY);
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(30, 2, 225, 25);
+
+        g.setColor(Color.WHITE);
+        Font font = new Font("Arial", Font.BOLD, 18);
+        g.setFont(font);
+        g.drawString("Comprar pasaje solo Ida", 35, 20);
     }
 }
--- ProyectoPrograDou/src/main/java/panelPrincipal/BotonVuelta.java
+++ ProyectoPrograDou/src/main/java/panelPrincipal/BotonVuelta.java
@@ -7,6 +7,18 @@ import java.awt.event.*;
 public class BotonVuelta extends JPanel {
     BotonVuelta(){
         this.setBounds(50,520,300,100);
-        this.setBackground(Color.GREEN);
+        this.setBackground(Color.lightGray);
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(10, 2, 280, 25);
+
+        g.setColor(Color.WHITE);
+        Font font = new Font("Arial", Font.BOLD, 18);
+        g.setFont(font);
+        g.drawString("Comprar pasaje de ida y vuelta", 15, 20);
     }
 }
