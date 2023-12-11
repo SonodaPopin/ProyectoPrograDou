b2cf34ce8c8a8ed10c0fd8f49c23d755ba45e972
SonodaPopin <joahernandez2022@udec.cl>
2023-12-11 04:06:53 -0300

    PanelGracias terminado

--- ProyectoPrograDou/src/main/java/panelFinal/PanelGracias.java
+++ ProyectoPrograDou/src/main/java/panelFinal/PanelGracias.java
@@ -1,13 +1,24 @@
 package panelFinal;
 
-import java.awt.Color;
+import java.awt.*;
 
 import javax.swing.*;
 
 public class PanelGracias extends JPanel{
 	public PanelGracias(){
-		this.setBounds(430,50,550,300);
-        this.setBackground(new Color(0, 0, 0, 0));
+		this.setBounds(430,115,550,150);
+        this.setBackground(new Color(100, 100, 100, 200));
 		this.setLayout(null);
+	}
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		g.setColor(Color.white);
+		Font font = new Font("Arial", Font.BOLD, 25);
+		g.setFont(font);
+		g.drawString("Su reserva a sido realizada con exito", 40, 40);
+		g.drawString("Gracias por preferir DouBus", 40, 80);
+		g.drawString("Buen viaje!!!", 40, 120);
 	}
 }
